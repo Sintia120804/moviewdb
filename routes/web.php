8788592e5c1e550c2779c2ad117cf429fd7d98ac
Sintia120804 <tia877805@gmail.com>
2025-05-26 16:34:24 +0700

    create movies

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,10 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', [MovieController::class, 'homepage']);
-
-Route::get('/movies/{id}', [MovieController::class, 'show'])->name('movies.detail');
-
+Route::get('/', [MovieController::class, 'homepage'])->name('homepage');
+Route::get('/movie/{id}', [MovieController::class, 'show'])->name('movies.detail');
 
 
+// Menampilkan form
+Route::get('/create-movie', [MovieController::class, 'create']);
+Route::post('/create-movie', [MovieController::class, 'store']);
